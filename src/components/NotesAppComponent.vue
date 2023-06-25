333a792a334a716738550b27b33b6b8c41c44f30
mehmet-bozdemir <mehmetbozdemir2000@gmail.com>
2023-06-25 18:34:20 +0200

    created a parent component

---
+++ src/components/NotesAppComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NotesComponent from "@/components/NotesComponent.vue";
+</script>
+
+<template>
+  <div class="container mx-auto p-4">
+    <NotesComponent/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
