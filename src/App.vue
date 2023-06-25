75a7f4296742c294f35058451bbb9deb73cb7d4b
mehmet-bozdemir <mehmetbozdemir2000@gmail.com>
2023-06-25 18:37:37 +0200

    refactored App.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import NotesComponent from "@/components/NotesComponent.vue";
+import {RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="container mx-auto">
+  <div class="container mx-auto p-4">
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-
-
       <div class="container mx-auto">
         <div class="mx-auto px-4 py-6">
           <h1 class="text-3xl text-center font-bold underline p-4">
@@ -18,20 +12,8 @@ import NotesComponent from "@/components/NotesComponent.vue";
           </h1>
         </div>
       </div>
-
-      <NotesComponent/>
-
-
-
-<!--      <nav>-->
-<!--        <RouterLink to="/">Home</RouterLink>-->
-<!--        <RouterLink to="/about">About</RouterLink>-->
-<!--      </nav>-->
     </div>
   </div>
-
-
-
 
   <RouterView />
 </template>
